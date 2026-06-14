7ec706f2cfead440129f2b55e142be22ef034307
Julia <upogorelova626@gmail.com>
2026-06-14 17:04:06 +0300

    feat: add team members search and team members task

--- src/app/features/projects/components/project-members-page-components/project-members/project-members.component.ts
+++ src/app/features/projects/components/project-members-page-components/project-members/project-members.component.ts
@@ -1,6 +1,7 @@
 import {
     ChangeDetectionStrategy,
     Component,
+    computed,
     inject,
     input,
     OnInit,
@@ -14,10 +15,16 @@ import {
     TuiTextfield
 } from '@taiga-ui/core';
 import {TuiAvatar} from '@taiga-ui/kit';
-import {finalize} from 'rxjs';
+import {finalize, forkJoin, startWith} from 'rxjs';
 import {ProjectMember} from '../../../interfaces/project-member.interface';
 import {ProjectsService} from '../../../services/projects.service';
 import {TuiSkeleton} from '@taiga-ui/kit';
+import {TeamRoleLabelPipe} from '../../../../../shared/pipes/team-role-label.pipe';
+import {Project} from '../../../interfaces/project.interface';
+import {ProjectTasksService} from '../../../services/project-tasks.service';
+import {ProjectTask} from '../../../interfaces/project-tasks.interface';
+import {FormControl, ReactiveFormsModule} from '@angular/forms';
+import {toSignal} from '@angular/core/rxjs-interop';
 
 @Component({
     selector: 'app-project-members',
@@ -28,7 +35,9 @@ import {TuiSkeleton} from '@taiga-ui/kit';
         TuiTextfield,
         TuiButton,
         TuiInput,
-        TuiSkeleton
+        TuiSkeleton,
+        TeamRoleLabelPipe,
+        ReactiveFormsModule
     ],
     templateUrl: './project-members.component.html',
     styleUrl: './project-members.component.less',
@@ -38,22 +47,55 @@ export class ProjectMembersComponent implements OnInit {
     readonly projectId = input.required<string>();
 
     private readonly projectsService = inject(ProjectsService);
+    private readonly projectTasksService = inject(ProjectTasksService);
 
     protected readonly isLoading = signal(false);
     protected readonly projectMembers = signal<ProjectMember[]>([]);
+    protected readonly tasks = signal<ProjectTask[]>([]);
 
     ngOnInit() {
-        this.loadProjectMembers();
-    }
-
-    private loadProjectMembers() {
         this.isLoading.set(true);
 
-        this.projectsService
-            .getProjectMembers(this.projectId())
-            .pipe(finalize(() => this.isLoading.set(false)))
-            .subscribe(projectMembers => {
+        forkJoin({
+            projectMembers: this.projectsService.getProjectMembers(
+                this.projectId()
+            ),
+            tasks: this.projectTasksService.getAllTasks(this.projectId())
+        })
+            .pipe(
+                finalize(() => {
+                    this.isLoading.set(false);
+                })
+            )
+            .subscribe(({projectMembers, tasks}) => {
                 this.projectMembers.set(projectMembers);
+                this.tasks.set(tasks);
             });
     }
+
+    protected taskCount(userId: string): number {
+        return this.tasks().filter(task => task.assignee?.id === userId).length;
+    }
+
+    protected readonly search = new FormControl('', {nonNullable: true});
+
+    private readonly searchQuery = toSignal(
+        this.search.valueChanges.pipe(startWith(this.search.value)),
+        {
+            initialValue: ''
+        }
+    );
+
+    protected members = computed(() => {
+        const searchQuery = this.searchQuery().trim().toLowerCase();
+        if (!searchQuery) {
+            return this.projectMembers();
+        }
+        return this.projectMembers().filter(
+            member =>
+                member.user.firstName?.toLowerCase().includes(searchQuery) ||
+                member.user.lastName?.toLowerCase().includes(searchQuery) ||
+                member.user.email.toLowerCase().includes(searchQuery)
+        );
+    });
 }
